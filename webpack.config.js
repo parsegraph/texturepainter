dd7ec140d3869fcdc46453fc3a652be3fdacb038
Aaron Faanes <dafrito@gmail.com>
2021-01-24 21:24:37 -0600

    Change how files are deployed

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   entry: path.resolve(__dirname, "src/TODO-PACKAGE-NAME.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "TODO-PACKAGE-NAME.js",
+    filename: "parsegraph-TODO-PACKAGE-NAME.js",
     globalObject: "this",
-    library: "TODO-PACKAGE-NAME",
+    library: "parsegraph",
     libraryTarget: "umd",
   },
   module: {
