5119cb179f81519f23c822952de352609f4d313e
Aaron Faanes <dafrito@gmail.com>
2021-09-10 19:28:47 -0500

    Add production

---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "dist-prod"),
+    filename: "parsegraph-TODO-PACKAGE-NAME.js",
+    globalObject: "this",
+    library: "parsegraph",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"]
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["ts-shader-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/inline"
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".glsl"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  mode: "production",
+};
