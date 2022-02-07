4d9cfceef5f5f6ebb551278dbce09dbc53daef07
Aaron Faanes <dafrito@gmail.com>
2022-02-07 14:32:47 -0600

    minor lint

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import { assert } from "chai";
 import TexturePainter from "../dist/parsegraph-texturepainter";
-import {matrixIdentity3x3} from 'parsegraph-matrix';
+import { matrixIdentity3x3 } from "parsegraph-matrix";
 
 import { mockDOM } from "node-canvas-webgl";
-import {BasicGLProvider} from "parsegraph-compileprogram";
+import { BasicGLProvider } from "parsegraph-compileprogram";
 import Color from "parsegraph-color";
 mockDOM(window);
 
@@ -14,12 +14,24 @@ describe("Package", function () {
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    const pixels = new Uint8Array(1024*1024*4);
-    for(let i = 0; i < pixels.length; ++i) {
+    const pixels = new Uint8Array(1024 * 1024 * 4);
+    for (let i = 0; i < pixels.length; ++i) {
       pixels[i] = Math.round(Math.random() * 255);
     }
 
-    win.gl().texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1024, 1024, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+    win
+      .gl()
+      .texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        1024,
+        1024,
+        0,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        pixels
+      );
     const painter = new TexturePainter(win, texture, 1024, 1024);
     painter.drawWholeTexture(0, 0, 512, 512, 0.5);
     painter.render(matrixIdentity3x3());
