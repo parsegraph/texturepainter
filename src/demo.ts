4294bf578efcbc5b760a605cd0c83b5c5dd3c60d
Aaron Faanes <dafrito@gmail.com>
2022-02-26 23:40:42 -0600

    Fix demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,24 +1,92 @@
-import todo from ".";
+import {BasicGLProvider} from "parsegraph-compileprogram";
+import TexturePainter from ".";
+import { make2DProjection } from "parsegraph-matrix";
+
+//
+// Initialize a texture and load an image.
+// When the image finished loading copy it into the texture.
+//
+function loadTexture(gl:WebGLRenderingContext, url:string) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be downloaded over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn off mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value:number) {
+  return (value & (value - 1)) == 0;
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
   root.style.position = "relative";
 
-  const container = document.createElement("div");
-  container.innerHTML = `${todo()}`;
+  const glProvider = new BasicGLProvider();
+
+  const container = glProvider.container();
   container.style.position = "absolute";
   container.style.left = "0px";
   container.style.top = "0px";
+  container.style.right = "0px";
+  container.style.bottom = "0px";
   container.style.pointerEvents = "none";
   root.appendChild(container);
   container.style.fontSize = "18px";
   container.style.fontFamily = "sans";
+
+  const gl = glProvider.gl();
+  const texture = loadTexture(gl, "image.png");
+  const painter = new TexturePainter(glProvider, texture, 256, 256);
   const refresh = () => {
-    const rand = () => Math.floor(Math.random() * 255);
-    document.body.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.color = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.left = `${Math.random() * root.clientWidth}px`;
-    container.style.top = `${Math.random() * root.clientHeight}px`;
+    painter.drawWholeTexture(Math.random() * glProvider.width(), Math.random() * glProvider.height(), 256, 256, 1.0);
+    const animate = ()=>{
+      gl.viewport(0, 0, glProvider.width(), glProvider.height());
+      const mat = make2DProjection(glProvider.width(), glProvider.height());
+      glProvider.render();
+      painter.render(mat);
+      requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
   };
 
   const dot = document.createElement("div");
@@ -42,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
     dotIndex = (dotIndex + 1) % dotState.length;
     dot.style.backgroundColor = dotState[dotIndex];
   };
-  const interval = 3000;
+  const interval = 1000;
   const dotInterval = 500;
   root.addEventListener("click", () => {
     if (timer) {
