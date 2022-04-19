f875994dd5a5cfb83e465a85532fd598aed86b62
Aaron Faanes <dafrito@gmail.com>
2022-04-19 17:00:18 -0500

    minor lint

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,4 @@
-import {BasicGLProvider} from "parsegraph-compileprogram";
+import { BasicGLProvider } from "parsegraph-compileprogram";
 import TexturePainter from ".";
 import { make2DProjection } from "parsegraph-matrix";
 
@@ -6,7 +6,7 @@ import { make2DProjection } from "parsegraph-matrix";
 // Initialize a texture and load an image.
 // When the image finished loading copy it into the texture.
 //
-function loadTexture(gl:WebGLRenderingContext, url:string) {
+function loadTexture(gl: WebGLRenderingContext, url: string) {
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
 
@@ -22,29 +22,43 @@ function loadTexture(gl:WebGLRenderingContext, url:string) {
   const border = 0;
   const srcFormat = gl.RGBA;
   const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                width, height, border, srcFormat, srcType,
-                pixel);
+  const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    width,
+    height,
+    border,
+    srcFormat,
+    srcType,
+    pixel
+  );
 
   const image = new Image();
-  image.onload = function() {
+  image.onload = function () {
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                  srcFormat, srcType, image);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      level,
+      internalFormat,
+      srcFormat,
+      srcType,
+      image
+    );
 
     // WebGL1 has different requirements for power of 2 images
     // vs non power of 2 images so check if the image is a
     // power of 2 in both dimensions.
     if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-       // Yes, it's a power of 2. Generate mips.
-       gl.generateMipmap(gl.TEXTURE_2D);
+      // Yes, it's a power of 2. Generate mips.
+      gl.generateMipmap(gl.TEXTURE_2D);
     } else {
-       // No, it's not a power of 2. Turn off mips and set
-       // wrapping to clamp to edge
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      // No, it's not a power of 2. Turn off mips and set
+      // wrapping to clamp to edge
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     }
   };
   image.src = url;
@@ -52,10 +66,9 @@ function loadTexture(gl:WebGLRenderingContext, url:string) {
   return texture;
 }
 
-function isPowerOf2(value:number) {
+function isPowerOf2(value: number) {
   return (value & (value - 1)) == 0;
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
@@ -78,14 +91,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const texture = loadTexture(gl, "image.png");
   const painter = new TexturePainter(glProvider, texture, 256, 256);
   const refresh = () => {
-    painter.drawWholeTexture(Math.random() * glProvider.width(), Math.random() * glProvider.height(), 256, 256, 1.0);
-    const animate = ()=>{
+    painter.drawWholeTexture(
+      Math.random() * glProvider.width(),
+      Math.random() * glProvider.height(),
+      256,
+      256,
+      1.0
+    );
+    const animate = () => {
       gl.viewport(0, 0, glProvider.width(), glProvider.height());
       const mat = make2DProjection(glProvider.width(), glProvider.height());
       glProvider.render();
       painter.render(mat);
       requestAnimationFrame(animate);
-    }
+    };
     requestAnimationFrame(animate);
   };
 
